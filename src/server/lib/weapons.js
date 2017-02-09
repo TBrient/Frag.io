b8c8f18eb13d5cb692daeae8c968137a6f6dc038
drewwww <andrew_eggleston@student.wayland.k12.ma.us>
2017-02-09 13:27:40 -0500

    Weapon constants and heartbeat listening

--- src/server/lib/weapons.js
+++ src/server/lib/weapons.js
@@ -1,3 +1,7 @@
-/**
- * Created by eggle on 2/7/2017.
- */
+'use strict';
+
+const WEAPON_FIST = 0;
+const WEAPON_PISTOL = 1;
+const WEAPON_M4 = 2;
+const WEAPON_SHOTGUN = 3;
+const WEAPON_ROCKETLAUNCHER = 4;
--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var weapons = require('./lib/weapons');
+
 /* Config file */
 var c = require('../../config.json');
 
@@ -24,10 +26,11 @@ function movePlayer(player) {
 }
 
 io.on('connection', function (socket) {
-    console.log('New user connected!', socket.handshake.query.type)
+    console.log('New user connected!', socket.handshake.query.type);
 
     var type = socket.handshake.query.type;
     var position = util.randomPosition();
+    var weapon = WEAPON_FIST;
 
     var cells = [];
     if(type === 'player'){
@@ -49,12 +52,64 @@ io.on('connection', function (socket) {
         target: {
             x: 0,
             y: 0
-        }
+        },
+        currentWeapon: weapon
     };
 
     socket.on('gotit', function (player) {
-        //TODO: What do we do when a player connects?
-        //1. get map array
-    })
-});
+        console.log('Connection from player name: '+player.name);
 
+        if(util.findIndex(users, player.id) > -1) { /* Check if the socket ID is already open on our side */
+            console.log('Player already connected, disconnecting ID '+player.id);
+            socket.disconnect()
+        } else if (!util.validNick(player.name)) { /* Player's selected name contains escape characters */
+            socket.emit('kick', 'Username contains invalid characters');
+            socket.disconnect()
+        } else { /* Player has connected successfully */
+            console.log('Player\'s connection was successful.');
+            sockets[player.id] = socket;
+
+            /* Set player's x and y to a random spawn position */
+            var pos = util.randomPosition();
+            player.x = pos.x;
+            player.y = pos.y;
+
+            currentPlayer = player;
+            currentPlayer.lastHeartbeat = new Date().getTime(); /* If user doesn't interact, heartbeat doesn't update */
+
+            /* Push the currentPlayer object to the users array */
+            users.push(currentPlayer);
+
+            /* Emit locally that a player with name 'name' has connected */
+            io.emit('playerJoin', { name: currentPlayer.name} );
+        }
+
+    });
+
+    /* Ping check; socket.io already has 'ping' and 'pong' so we use 'drip' and 'drop' */
+    socket.on('drip', function () {
+        socket.emit('drop');
+    });
+
+    socket.on('disconnect', function () {
+        if (util.findIndex(users, currentPlayer.id) > -1)
+            users.splice(util.findIndex(users, currentPlayer.id), 1);
+        console.log('User ' + currentPlayer.name + ' disconnected.');
+
+        socket.broadcast.emit('playerDisconnect', { name: currentPlayer.name }); // Use broadcast so we don't send data to a closing socket
+    });
+
+    /* Refresh info */
+    socket.on('0', function(target) {
+        currentPlayer.lastHeartbeat = new Date().getTime();
+        if (target.x !== currentPlayer.x || target.y !== currentPlayer.y) {
+            currentPlayer.target = target;
+        }
+    });
+
+    /* Fired a weapon */
+    socket.on('1', function() {
+        //TODO: use current weapon to 'fire' a bullet
+    });
+
+});
