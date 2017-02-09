05b2b341350b3bbf98df5a988b686143052dc943
Razer0901 <geoffreywwang@gmail.com>
2017-02-09 11:18:32 -0500

    Commented main.js

--- src/client/main.js
+++ src/client/main.js
@@ -3,7 +3,7 @@ var KEY_SPACE = 32, KEY_A = 65, KEY_W = 87, KEY_D = 68, KEY_S = 83, KEY_LEFT = 3
 
 //Setup
 var stage; //The object we draw/animate on (Canvas)
-var keys = {}; //Stores all the keypresses
+var keys = {}; //Stores all keyevents
 
 //World
 var world;
@@ -13,37 +13,56 @@ var world;
  * @constructor
  */
 function Main() {
-    stage = new createjs.Stage("Canvas"); //Point and set the stage to corrispond to the "Canvas" in the HTML
+    stage = new createjs.Stage("Canvas"); //Point and set the stage to correspond to the "Canvas" in the HTML
 
     //Set up the world
     world = new World();
     world.addPlayer(new Player({x:100,y:100},0,65,"red")); //Add a player
     world.addPlayer(new Player({x:400,y:200},0,65,"blue")); //Add another player
-
     addWorldToStage(world);
 
+    //Set framerate and update method
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", update);
 
+    //Keyevents
     this.document.onkeydown = keydown;
     this.document.onkeyup = keyup;
 }
 
+/**
+ * Called each frame to update everything
+ */
 function update(){
     world.update(keys);
-    stage.update();
+    stage.update(); //Repaint
 }
 
+/**
+ * Add all the objects from a world to the stage
+ * @param world
+ */
+function addWorldToStage(world) {
+    world.players.forEach(function (player) {
+        stage.addChild(player.node);
+    });
+}
+
+//-------------------- Utility Functions --------------------
+
+/**
+ * Sets keys accordingly when a key is pressed
+ * @param event
+ */
 function keydown(event) {
     keys[event.keyCode] = true;
 }
 
+
+/**
+ * Sets keys accordingly when a key is released
+ * @param event
+ */
 function keyup(event) {
     delete keys[event.keyCode];
-}
-
-function addWorldToStage(world){
-    world.players.forEach(function (player, index) {
-        stage.addChild(player.node);
-    });
 }
